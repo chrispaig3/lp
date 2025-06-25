6dda17e18c6fe50fa814128893a81645b9707663
chrispaig3 <v0idmatr1x.0@gmail.com>
2025-06-25 18:43:23 -0500

    add experimental branch with registry data structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,25 @@
+use serde::Deserialize;
 use std::fs;
 use toml;
-use serde::Deserialize;
+
+pub trait Registry {
+    fn push_all(&self, registry: Vec<Plugin>) -> Option<Vec<Plugin>>;
+}
+
+impl<T: PluginManager> Registry for T {
+    fn push_all(&self, registry: Vec<Plugin>) -> Option<Vec<Plugin>> {
+        let mut v = Vec::new();
+
+        for plugin in registry {
+            v.push(plugin);
+            return Some(v);
+        }
+        None
+    }
+}
 
 pub trait PluginManager {
-    fn register(&self, f: impl Fn(Plugin));
+    fn register(&self, f: impl Fn(Plugin)) -> Plugin;
     fn unregister(&self, f: impl Fn(Plugin));
     fn run(&self, f: impl Fn(Plugin));
 }
@@ -13,7 +29,7 @@ pub trait PluginManager {
 pub struct Toml {
     pub plugin: Plugin,
 }
- 
+
 // Ron Manifest
 #[derive(Debug, Deserialize)]
 pub struct Ron {
@@ -39,8 +55,9 @@ pub struct Plugin {
 // Plugin Management
 impl PluginManager for Plugin {
     /// Registers the plugin, adding it to the appropriate directory
-    fn register(&self, f: impl Fn(Plugin)) {
+    fn register(&self, f: impl Fn(Plugin)) -> Plugin {
         f(self.clone());
+        self.clone()
     }
 
     /// Unregisters the plugin, removing it from the directory
@@ -84,13 +101,13 @@ mod tests {
         let toml = Toml::parse("test_asset/plugin.toml").unwrap();
         let ron = Ron::parse("test_asset/plugin.ron").unwrap();
         let json = Json::parse("test_asset/plugin.json").unwrap();
-
         assert_eq!(ron.plugin.name, "test_asset");
         assert_eq!(json.plugin.name, "test_asset");
-        
-        toml.plugin.register(|plugin| {
+
+        // Register plugin with custom logic.
+        let p = toml.plugin.register(|plugin| {
             if let Some(path) = &plugin.path {
-               if !Path::new(path).exists() {
+                if !Path::new(path).exists() {
                     println!("Plugin directory does not exist, creating @: {:?}", path);
                     // set up plugin dir
                     // move plugin files to dir
@@ -100,5 +117,14 @@ mod tests {
                 }
             }
         });
+
+        // Attach plugins to registry
+        let plugins = vec![p];
+        let registry = toml
+            .plugin
+            .push_all(plugins)
+            .expect("Failed to push plugin to registry");
+
+        assert!(registry.len() >= 1)
     }
 }

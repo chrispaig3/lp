7f64884bd620d5937ad179864cf0e87fe1d0aaab
Chris Paige <88049272+chrispaig3@users.noreply.github.com>
2025-07-09 11:55:19 -0500

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! impl_parse {
     // This macro takes two arguments:
-    // 1. $struct_name:ident - The identifier for the struct (e.g., Toml, Ron, Json)
+    // 1. $struct:ident - The identifier for the struct (e.g., Toml, Ron, Json)
     // 2. $parser_fn:path - The path to the parsing function (e.g., toml::from_str, ron::from_str)
     ($struct:ident, $parser_fn:path) => {
         impl $struct {

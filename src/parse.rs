00b9c060677d3b72660c178e8f539c1e1e4a94e6
chrispaig3 <v0idmatr1x.0@gmail.com>
2025-07-09 11:42:51 -0500

    cleanup

--- src/parse.rs
+++ src/parse.rs
@@ -3,8 +3,8 @@ macro_rules! impl_parse {
     // This macro takes two arguments:
     // 1. $struct_name:ident - The identifier for the struct (e.g., Toml, Ron, Json)
     // 2. $parser_fn:path - The path to the parsing function (e.g., toml::from_str, ron::from_str)
-    ($struct_name:ident, $parser_fn:path) => {
-        impl $struct_name {
+    ($struct:ident, $parser_fn:path) => {
+        impl $struct {
             pub fn parse(path: &str) -> Result<Self, Box<dyn std::error::Error>> {
                 let content = fs::read_to_string(path)?;
                 $parser_fn(&content).map_err(|e| e.into())

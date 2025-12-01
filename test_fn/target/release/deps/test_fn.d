13c77b3d74d647f5e34608cfe51c22533632508c
Chris Paige <88049272+chrispaig3@users.noreply.github.com>
2025-12-01 01:49:36 -0500

    Delete test_fn/target directory

--- test_fn/target/release/deps/test_fn.d
+++
@@ -1,5 +0,0 @@
-/Users/chris/Dev/lp/test_fn/target/release/deps/test_fn.d: src/lib.rs
-
-/Users/chris/Dev/lp/test_fn/target/release/deps/libtest_fn.dylib: src/lib.rs
-
-src/lib.rs:
--- test_fn/target/release/libtest_fn.d
+++
@@ -1,1 +0,0 @@
-/Users/chris/Dev/lp/test_fn/target/release/libtest_fn.dylib: /Users/chris/Dev/lp/test_fn/src/lib.rs
